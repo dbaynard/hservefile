157b8f10dfad6db1b49b5d0598203ace6aff015b
David Baynard <davidbaynard@gmail.com>
2015-09-23 20:17:49 +0100

    Hello World

---
+++ src/Main.hs
@@ -0,0 +1,17 @@
+module Main (
+    main
+)   where
+
+import Network.Wai (responseLBS, Application)
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
+
+main = do
+    let port = 3000
+    putStrLn $ "Listening on port " ++ show port
+    run port app
+
+app :: Application
+app req f =
+    f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
